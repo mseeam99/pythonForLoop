5cb8b087b4f586a83d0a54d55ca808cd4e9320a5
Mohammad Seeam <112004640+mseeam99@users.noreply.github.com>
2023-11-21 00:57:12 -0600

    Add files via upload

---
+++ pythonForLoop.py
@@ -0,0 +1,30 @@
+sentence = "HELLOWORLD"
+
+print("Length is :" , len(sentence))
+
+print()
+
+for i in range(len(sentence)):
+    print(sentence[i])
+    
+print()
+    
+for i in range(len(sentence)):
+    print(i)
+    
+print()
+
+myList = []
+
+for i in range(5):
+    myList.append(i)
+    
+print(myList)
+
+print()
+
+for i in myList:
+    print(i)
+    
+    
+    
